fecdc639abaf40daad2a4c667841cb4b2c3a14c3
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-13 01:33:37 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, MongoRuntimeError } = require('mongodb');
 const app = express()
 const port = process.env.Port || 5000;
+
+// middle Wire
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blwbb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Database connected')
+    }
+    finally {
+
+    }
+}
+
+run(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello From Doctor Uncle')
