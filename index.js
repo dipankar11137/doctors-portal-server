03a97f533e44e250dd5119c11683a44449ddeb4f
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-19 19:03:37 +0600

    delete doctor

--- index.js
+++ index.js
@@ -178,12 +178,25 @@ async function run() {
             return res.send({ success: true, result });
         });
 
-        // add doctor
+        // add doctor in mongodb
         app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
             const doctor = req.body;
             const result = await doctorCollection.insertOne(doctor);
             res.send(result);
-        })
+        });
+
+        // Get doctor from mongodb
+        app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+            const doctors = await doctorCollection.find().toArray();
+            res.send(doctors);
+        });
+        // Delete a doctor from mongodb
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await doctorCollection.deleteOne(filter);
+            res.send(result);
+        });
 
     }
     finally {
